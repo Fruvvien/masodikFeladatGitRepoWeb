480ce464213ffdb16efdda07fe1f917887b85752
Gabi <HunFantasztik@gmail.com>
2024-03-22 14:44:36 +0100

    ez egy gomb

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+
+
+    const gomb = document.getElementById("button");
+
+    gomb.addEventListener("click", () =>{
+        alert("Hello World!")
+    })
+
+})
